87837e9bd1551413ec6ec91e76cc1eb0dec83cb1
Zubair Atifgit config --global user.email zubair.atif@@gmail.comgit config --global user.name Zubair <zubair.atif@gmail.com>
2023-03-01 22:02:44 +0000

    Updated / cleaned  09_Palindromes!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,6 @@
 const palindromes = function (str) {
-  //   let lowRegStr = str.toLowerCase().replace(/[\W_]/g, "");
-  //   let reverseStr = lowRegStr.split("").reverse().join("");
-  //   return reverseStr === lowRegStr;
   let refinedStr = str.toLowerCase().replace(/[\W_]/g, "");
-  return (
-    str.toLowerCase().replace(/[\W_]/g, "").split("").reverse().join("") ===
-    refinedStr
-  );
+  return refinedStr.split("").reverse().join("") === refinedStr;
 };
 
 // Do not edit below this line
