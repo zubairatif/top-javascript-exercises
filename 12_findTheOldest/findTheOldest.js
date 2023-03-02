4248ddbfa51b5497ce32f4fd61141cf167038d68
Zubair Atifgit config --global user.email zubair.atif@@gmail.comgit config --global user.name Zubair <zubair.atif@gmail.com>
2023-03-02 22:11:50 +0000

    completed findTheOldest!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+  let oldestPerson;
+  let oldestAge = 0;
+  let age;
+  for (let i = 0; i < persons.length; i++) {
+    "yearOfDeath" in persons[i]
+      ? (age = persons[i].yearOfDeath - persons[i].yearOfBirth)
+      : (age = new Date().getFullYear() - persons[i].yearOfBirth);
+    if (age > oldestAge) {
+      oldestPerson = persons[i];
+      oldestAge = age;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
